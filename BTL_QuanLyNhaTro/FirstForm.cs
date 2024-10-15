20104f539ccc6131fc09e43f1a03c238febb2064
Xuan Diep <xuandiep003@gmail.com>
2024-10-15 14:50:30 +0700

    add_project

---
+++ BTL_QuanLyNhaTro/FirstForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using static BTL_QuanLyNhaTro.AuthenticationService;
+
+namespace BTL_QuanLyNhaTro
+{
+    public partial class FirstForm : Form
+    {
+        public FirstForm(User user)
+        {
+            InitializeComponent();
+            ApplyRolePermissions(user.RoleID);
+        }
+        private void ApplyRolePermissions(int roleID)
+        {
+            switch (roleID)
+            {
+                case 1: // KhachThue
+                    mApp_mService_A.Visible = false;
+                    mApp_setRoleAccount_A.Visible = false;
+                    mApp_mRoom_R.Visible = false;
+                    mApp_mUser_R.Visible = false;
+                    mApp_aBill.Visible = false;
+                    mApp_aContract.Visible = false;
+                    mApp_vRevenue_R.Visible = false;
+                    break;
+                case 2: // ChuTro
+                    mApp_mService_A.Visible = false;
+                    mApp_setRoleAccount_A.Visible = false;
+                    mApp_rRoom.Visible = false;
+                    break;
+                case 3: // Admin
+                    break;
+            }
+
+        }
+
+        private void mApp_aBill_Click(object sender, EventArgs e)
+        {
+            FormThemHoaDon f = new FormThemHoaDon();
+            f.Show();
+        }
+    }
+}
---
+++ BTL_QuanLyNhaTro/WelcomeForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static BTL_QuanLyNhaTro.FunctionAll;
+using static BTL_QuanLyNhaTro.AuthenticationService;
+
+namespace BTL_QuanLyNhaTro
+{
+    public partial class WelcomeForm : Form
+    {
+
+        public WelcomeForm()
+        {
+            InitializeComponent();
+            cb_show.MouseDown += holdCheckBox_MouseDown;
+            cb_show.MouseUp += holdCheckBox_MouseUp;
+            cb_show_re.MouseDown += holdCheckBox_MouseDown;
+            cb_show_re.MouseUp += holdCheckBox_MouseUp;
+        }
+        private void holdCheckBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            cb.Checked = true;
+        }
+
+        private void holdCheckBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            cb.Checked = false;
+        }
+
+        private void bt_re_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cb_show_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            tb_pass.UseSystemPasswordChar = !cb.Checked;
+            tb_pass_register.UseSystemPasswordChar = !cb.Checked;
+            tb_confirm_pass.UseSystemPasswordChar = !cb.Checked;
+        }
+
+        public void Login(string username, string password)
+        {
+            AuthenticationService authService = new AuthenticationService();
+            User user = authService.Authenticate(username, password);
+
+            if (user != null)
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+                FirstForm f = new FirstForm(user);
+                f.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng.");
+            }
+        }
+        private void bt_login_Click(object sender, EventArgs e)
+        {
+            string username = tb_name.Text;
+            string password = tb_pass.Text;
+            Login(username, password);
+        }
+    }
+}
